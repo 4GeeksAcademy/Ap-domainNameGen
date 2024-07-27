27df9aa9c99444c01d40d96753da697885c760d8
Adrian <adrian.pina11@gmail.com>
2024-07-27 00:09:49 +0000

    Domain NG

--- src/app.js
+++ src/app.js
@@ -1,11 +1,28 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
+let pronoun = ["the", "our"];
+let adj = ["great", "big"];
+let noun = ["jogger", "racoon"];
+let extensions = ["com", "net", "us", "io"];
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+function generateDomains(pronoun, adj, noun, extensions) {
+  let domains = [];
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+  for (let p of pronoun) {
+    for (let a of adj) {
+      for (let n of noun) {
+        for (let ext of extensions) {
+          domains.push(`${p}${a}${n}.${ext}`);
+
+          if (n.endsWith(ext)) {
+            let hackedDomain = `${p}${a}${n.slice(0, -ext.length)}.${ext}`;
+            domains.push(hackedDomain);
+          }
+        }
+      }
+    }
+  }
+
+  return domains;
+}
+
+let allDomains = generateDomains(pronoun, adj, noun, extensions);
+allDomains.forEach(domain => console.log(domain));
